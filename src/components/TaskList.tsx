df61d0cff7434b92a4bde9326341c6faff0c91d0
FeruYasu <fernando_yasumoto@hotmail.com>
2021-03-15 13:06:12 -0300

    Finished challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,36 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [counter, setCounter] = useState(0);
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle){
+      const newTask = {
+        id: counter,
+        title: newTaskTitle,
+        isComplete: false,
+      }
+  
+      const newCounter = counter + 1;
+      setCounter(newCounter);
+  
+      setTasks([...tasks, newTask])
+    }
+
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const index = tasks.findIndex((el) => {return el.id === id});
+
+    if (index !== -1){
+      tasks[index].isComplete = !tasks[index].isComplete
+      setTasks([...tasks]);
+    }
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
